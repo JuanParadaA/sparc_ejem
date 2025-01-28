00031bd2f31eb1007425df375ca41909098e9024
JuanParadaA <jeparadaa@udistrital.edu.co>
2025-01-28 09:07:49 -0500

    Update viscosity.s

--- viscosity.s
+++ viscosity.s
@@ -1,20 +1,30 @@
-    !Asumiendo que los valores estan cargados en los respectivos espacios
-    mov pos, %l0  
-    mov vel, %l1   
-    mov viscosidad, %l2  
-    mov tiempo, %l3 
-    mov pasos, %l4   
+.equ POS_INIT, 100
+.equ VEL_INIT, 50
+.equ VISCOSIDAD_INIT, 2
+.equ TIEMPO_INIT, 1
+.equ PASOS_INIT, 1000
+
+.global main
+main:
+  mov POS_INIT, %l0       ! pos = 100
+  mov VEL_INIT, %l1       ! vel = 50
+  mov VISCOSIDAD_INIT, %l2  ! b = 2
+  mov TIEMPO_INIT, %l3    ! dt = 1
+  mov PASOS_INIT, %l4     ! steps = 1000
 
 ciclo:
-    smul %l2, %l1, %l5   !F = -b * v
-    sub %g0, %l5, %l5    
-    
-    smul %l5, %l3, %l5   !v = v + F
-    add %l1, %l5, %l1    
+  smul %l2, %l1, %l5   ! F = -b * v 
+  sub %g0, %l5, %l5    ! F = -b * v 
 
-    smul %l1, %l3, %l5   !p += (v * dt)
-    add %l0, %l5, %l0   
+  smul %l5, %l3, %l5   ! F * dt
+  add %l1, %l5, %l1    ! vel += F * dt
 
-    subcc %l4, 1, %l4
+  smul %l1, %l3, %l5   ! v * dt
+  add %l0, %l5, %l0    ! pos += vel * dt
+
+  subcc %l4, 1, %l4    ! 
+  bne ciclo            ! != 0
+  smul %l2, %l1, %l5   ! Inicia F = -b*v para la siguiente iteracion
     bne ciclo
-    nop
+final:
+  nop
