b20ff2a5ae06f09376875512e082ab717d12f36a
JuanParadaA <jeparadaa@udistrital.edu.co>
2025-01-28 08:17:22 -0500

    Update viscosity.s

--- viscosity.s
+++ viscosity.s
@@ -7,9 +7,9 @@
 ciclo:
     smul %l2, %l1, %l5   !F = -b * v
     sub %g0, %l5, %l5    
-    smul %l5, %l3, %l5  
-   
-    add %l1, %l5, %l1    !v = v + F
+    
+    smul %l5, %l3, %l5   !v = v + F
+    add %l1, %l5, %l1    
 
     smul %l1, %l3, %l5   !p += (v * dt)
     add %l0, %l5, %l0   
