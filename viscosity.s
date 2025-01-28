ab449868131251325eebc33c958848b75a6e2d16
JuanParadaA <jeparadaa@udistrital.edu.co>
2025-01-28 09:34:18 -0500

    Update viscosity.s

--- viscosity.s
+++ viscosity.s
@@ -1,16 +1,16 @@
-.equ POS_INIT, 100
-.equ VEL_INIT, 50
-.equ VISCOSIDAD_INIT, 2
-.equ TIEMPO_INIT, 1
-.equ PASOS_INIT, 1000
-
+  define (POS_INIT, 0)         
+  define (VEL_INIT, 500)       
+  define (VISCOSIDAD_INIT, 1000)     
+  define (TIEMPO_INIT, 10)       
+  define (PASOS_INIT, 1)
+  
 .global main
 main:
-  mov POS_INIT, %l0       ! pos = 100
-  mov VEL_INIT, %l1       ! vel = 50
-  mov VISCOSIDAD_INIT, %l2  ! b = 2
-  mov TIEMPO_INIT, %l3    ! dt = 1
-  mov PASOS_INIT, %l4     ! pasos = 1000
+  mov POS_INIT, %l0      
+  mov VEL_INIT, %l1       
+  mov VISCOSIDAD_INIT, %l2 
+  mov TIEMPO_INIT, %l3   
+  mov PASOS_INIT, %l4     
 
 ciclo:
   smul %l2, %l1, %l5   ! F = -b * v 
